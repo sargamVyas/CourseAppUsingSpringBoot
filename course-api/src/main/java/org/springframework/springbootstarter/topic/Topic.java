84b33c1cbc780c1156fd0df044cc530982e336fd
Sargam Vyas <sargam.shukla12@gmail.com>
2020-09-22 23:07:33 +0200

    Change made to match clean code standards.

--- course-api/src/main/java/org/springframework/springbootstarter/topic/Topic.java
+++ course-api/src/main/java/org/springframework/springbootstarter/topic/Topic.java
@@ -2,16 +2,16 @@ package org.springframework.springbootstarter.topic;
 
 public class Topic {
 	
-	
 	private String id;
 	private String name;
 	private String description;
 	
-	public Topic() {
-		
-	}
+	public Topic() { }
 	
-	public Topic(String id, String name, String description) {
+	public Topic(String id, 
+			String name,
+			String description) 
+	{
 		super();
 		this.id = id;
 		this.name = name;
@@ -21,12 +21,15 @@ public class Topic {
 	public String getId() {
 		return id;
 	}
+	
 	public void setId(String id) {
 		this.id = id;
 	}
+	
 	public String getName() {
 		return name;
 	}
+	
 	public void setName(String name) {
 		this.name = name;
 	}
@@ -34,9 +37,9 @@ public class Topic {
 	public String getDescription() {
 		return description;
 	}
+	
 	public void setDescription(String description) {
 		this.description = description;
 	}
-	
 	
 }
--- course-api/src/main/java/org/springframework/springbootstarter/topic/TopicService.java
+++ course-api/src/main/java/org/springframework/springbootstarter/topic/TopicService.java
@@ -9,7 +9,8 @@ import org.springframework.stereotype.Service;
 @Service
 public class TopicService {
 	
-	private List<Topic> topics= new ArrayList<>(Arrays.asList(new Topic("Spring","Spring FrameWork","Sprin FrameWork Description"),
+	private List<Topic> topics= new ArrayList<>(Arrays.asList(
+			new Topic("Spring","Spring FrameWork","Sprin FrameWork Description"),
 			new Topic("Core","Core Java","Core Java Description"),
 			new Topic("SpringBoot","Spring Boot FrameWork","Spring Boot Description")));
 	
@@ -20,15 +21,19 @@ public class TopicService {
 	
 	public Topic getTopic(String id)
 	{
-		return topics.stream().filter(t->t.getId().equals(id)).findFirst().get();
+		return topics.stream().
+				filter(t->t.getId().
+				equals(id)).
+				findFirst().get();
 	}
 
-	public void addTopic(Topic topic) {
+	public void addTopic(Topic topic) 
+	{
 		topics.add(topic);
-		
 	}
 
-	public void updateTopic(String id,Topic topic) {
+	public void updateTopic(String id,Topic topic)
+	{
 		for(int i=0;i<topics.size();i++)
 		{
 			Topic t=topics.get(i);
@@ -38,7 +43,6 @@ public class TopicService {
 				return;
 			}
 		}
-		
 	}
 
 	public void deleteTopic(String id) {
